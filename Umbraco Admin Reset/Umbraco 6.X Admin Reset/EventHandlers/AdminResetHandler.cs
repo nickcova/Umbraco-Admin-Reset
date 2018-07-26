7bd310803176d0f116364a7e84b2478e69ba4676
Nicolas Cova <nicolas.cova.work@gmail.com>
2018-07-25 21:16:27 -0400

    Almost ready
    Added a new Exception, reading settings from the web.config file and deleting the DLL after making changes to the admin user.

--- Umbraco Admin Reset/Umbraco 6.X Admin Reset/EventHandlers/AdminResetHandler.cs
+++ Umbraco Admin Reset/Umbraco 6.X Admin Reset/EventHandlers/AdminResetHandler.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.IO;
 using System.Web;
+using System.Xml;
 using Umbraco.Core;
 using Umbraco.Core.Logging;
 using Umbraco.Core.Models.Membership;
 using Umbraco.Core.Services;
+using Umbraco_6.X_Admin_Reset.Exceptions;
 
 namespace Umbraco_6.X_Admin_Reset.EventHandlers
 {
@@ -26,37 +28,135 @@ namespace Umbraco_6.X_Admin_Reset.EventHandlers
             adminPassword = GetValueFromConfigFile("UmbracoAdminResetPassword", adminPassword);
 
             // Check for password modification permission
+            try
+            {
+                CheckForManualPasswordChange();
+            } catch (MembershipProviderMissingException mpme)
+            {
+                LogHelper.Error(GetType(), mpme.Message, mpme);
+                return;
+            }
 
             // Update admin info
             if (!string.IsNullOrEmpty(adminUsername) && !string.IsNullOrEmpty(adminPassword))
             {
-                try {
+                try
+                {
                     IUserService userService = ApplicationContext.Current.Services.UserService;
                     IUser adminUser = userService.GetUserById(0);
 
-                    if (adminUser != null) {
+                    if (adminUser != null)
+                    {
                         userService.SavePassword(adminUser, adminPassword);
                         adminUser.Username = adminUsername;
                         userService.Save(adminUser, false);
-                    } else
+                    }
+                    else
                     {
-                        // Write error message?
-                    }                    
-                } catch (NotSupportedException nse)
+                        LogHelper.Warn(GetType(), "How can you not have a user with ID=0?!.");
+                    }
+                }
+                catch (NotSupportedException nse)
                 {
                     LogHelper.Error(GetType(), "Failed to save the admin's password.", nse);
-                }                
-            } else 
+                }
+            }
+            else
             {
                 LogHelper.Warn(GetType(), "Something went wrong. Empty username or password");
             }
 
             // Delete .dll file
+            DeleteMyDll();
         }
+
 
         private string GetValueFromConfigFile(string settingName, string currentValue)
         {
-            throw new NotImplementedException();
+            string settingValue = string.Empty;
+
+            try
+            {
+                settingValue = ConfigurationManager.AppSettings[settingName];
+
+                if (!string.IsNullOrEmpty(settingValue))
+                {
+                    return settingValue;
+                }
+            } catch (ConfigurationErrorsException cee)
+            {
+                LogHelper.Error(GetType()
+                                , string.Format("Failed reading \"{0}\" from the AppSettings section of the web.config file.", settingName)
+                                , cee);
+            }
+
+            return currentValue;
+        }
+
+
+        private void CheckForManualPasswordChange()
+        {
+            try
+            {
+                //Create a new xml document
+                XmlDocument document = new XmlDocument
+                {
+                    //Keep current indentions format
+                    PreserveWhitespace = true
+                };
+
+                //Load the web.config file into the xml document
+                string webconfigPath = HttpContext.Current.Server.MapPath("~/web.config");
+                document.Load(webconfigPath);
+
+                XmlNode userMembershipNode =
+                    document.SelectSingleNode(
+                        "//configuration/system.web/membership/providers/add[@name='UsersMembershipProvider']");
+
+                if (userMembershipNode == null) {
+                    throw new MembershipProviderMissingException("Couldn't find the UsersMembershipProvider tag in the web.config file.");
+                }
+
+                XmlAttribute att = userMembershipNode.Attributes["allowManuallyChangingPassword"];
+
+                if (att == null)
+                {
+                    att = document.CreateAttribute("allowManuallyChangingPassword");
+                    userMembershipNode.Attributes.Append(att);
+                }
+                if (!att.Value.Equals("true"))
+                {
+                    att.Value = "true";
+                }
+
+                document.Save(webconfigPath);
+            }
+            catch (MembershipProviderMissingException mpme) {
+                throw mpme;
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(GetType(), "Error while checking for \"allowManuallyChangingPassword\".", ex);
+            }
+        }
+
+
+        private void DeleteMyDll()
+        {
+            string dllFilePath = HttpContext.Current.Server.MapPath("~/bin/Umbraco626AdminReset.dll");
+
+            if (!string.IsNullOrEmpty(dllFilePath) && File.Exists(dllFilePath))
+            {
+                try
+                {
+                    File.Delete(dllFilePath);
+                }catch (Exception exc)
+                {
+                    LogHelper.Error(GetType(), "I could not delete my DLL file.", exc);
+                }                
+            }
+
+            return;
         }
     }
 }
---
+++ Umbraco Admin Reset/Umbraco 6.X Admin Reset/Exceptions/MembershipProviderMissingException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Umbraco_6.X_Admin_Reset.Exceptions
+{
+    public class MembershipProviderMissingException : Exception
+    {
+        public MembershipProviderMissingException()
+        {
+        }
+
+        public MembershipProviderMissingException(string message)
+        : base(message)
+        {
+        }
+
+        public MembershipProviderMissingException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
